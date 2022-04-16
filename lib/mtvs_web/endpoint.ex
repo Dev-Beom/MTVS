540580fa952a90322cf84a022738d08a438b29e7
Dev-Beom <youngbeom0908@gmail.com>
2022-04-17 02:44:36 +0900

    Test Chat Socket Communication Code

--- lib/mtvs_web/endpoint.ex
+++ lib/mtvs_web/endpoint.ex
@@ -1,6 +1,8 @@
 defmodule MtvsWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :mtvs
 
+  socket "/socket", MtvsWeb.UserSocket, websocket: true, longpoll: false
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
